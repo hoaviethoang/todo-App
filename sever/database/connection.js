d7cd1a4020f059a03ef5d03782cc33cc6c089cde
ntheanh201 <theanh.nguyen@vietkentech.com>
2022-06-25 23:14:23 +0700

    env PORT

--- sever/database/connection.js
+++ sever/database/connection.js
@@ -5,6 +5,7 @@ const connectDB = () => {
     const con = mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
+      port: process.env.DB_PORT,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
     });
