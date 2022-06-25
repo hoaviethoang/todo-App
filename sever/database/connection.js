90d0ff9c86fea0021b1a6356e7c6d57723f1aa79
ntheanh201 <theanh.nguyen@vietkentech.com>
2022-06-25 23:10:13 +0700

    update server

--- sever/database/connection.js
+++ sever/database/connection.js
@@ -1,24 +1,23 @@
-const mysql = require('mysql');
-require('dotenv').config();
-const connectDB = () =>{
-  return new Promise((resolve,reject) =>{
+const mysql = require("mysql");
+require("dotenv").config();
+const connectDB = () => {
+  return new Promise((resolve, reject) => {
     const con = mysql.createConnection({
-      host: process.env.DB_HOST ,
-      port: 33306,
-      user: process.env.DB_USER ,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
     });
-    con.connect((err) =>{
-      if(err){
+    con.connect((err) => {
+      if (err) {
         reject(err);
       }
-      console.log("da ket noi thanh cong")
-      resolve(con); 
+      resolve(con);
+      console.log("da ket noi thanh cong");
     });
   });
 };
 module.exports = connectDB;
 module.exports.closeB = (con) => {
   con.destroy();
-}
+};
